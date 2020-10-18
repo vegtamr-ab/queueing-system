0b19fa8bf813ec87356723e98211aec2f8f05c49
vegtamr <barekov.am.v@gmail.com>
2020-10-18 17:43:10 +0300

    Added some placeholder values

--- src/queueing-system/simulation.rs
+++ src/queueing-system/simulation.rs
@@ -25,52 +25,57 @@ fn get_new_state(s: Simulation) -> State {
     match s.current_event {
         SimulationEvent::StopSimulation => s.state,
         SimulationEvent::NewRequest => State {
-            sources: s.state.sources.,
+            sources: s.state.sources/************ */,
             max_sources: s.state.max_sources,
             devices: s.state.devices,
             device_pointer: s.state.device_pointer,
             max_devices: s.state.max_devices,
-            buf: ,
-            buf_pointer: ,
+            buf: Vec::new(),
+            buf_pointer: 0,
             buf_max_length: s.state.buf_max_length,
             next_idle_at: s.state.devices.iter().max().expect("No devices"),
             next_any_idle_at: s.state.devices.iter().min().expect("No devices"),
             next_arrival_at: s.state.sources.iter().min().expect("No sources"),
             requests_left: s.state.requests_left,
-            requests_denied: ,
-            total_time_spent_in_system: ,
+            requests_denied: 0,
+            total_time_spent_in_system: 0,
             total_times_devices_busy: s.state.total_time_devices_busy,
         },
         SimulationEvent::ProcessRequest => State {
             sources: s.state.sources,
             max_sources: s.state.max_sources,
-            devices: ,
-            device_pointer: ,
+            devices: Vec::new(),
+            device_pointer: 0,
             max_devices: s.state.max_devices,
-            buf: ,
-            buf_pointer: ,
+            buf: Vec::new(),
+            buf_pointer: 0,
             buf_max_length: s.state.buf_max_length,
             next_idle_at: s.state.devices.iter().max().expect("No devices"),
             next_any_idle_at: s.state.devices.iter().min().expect("No devices"),
             next_arrival_at: s.state.sources.iter().min().expect("No sources"),
             requests_left: s.state.requests_left - 1,
-            requests_denied: ,
-            total_time_spent_in_system: ,
-            total_times_devices_busy: ,
+            requests_denied: 0,
+            total_time_spent_in_system: 0,
+            total_times_devices_busy: 0,
         },
     }
 }
 
-fn get_new_buffer(st: State) -> (Vec<u64>, u32) {
+fn get_new_buffer(st: State) -> (Vec<Option<u64>>, u32) {
     match st.next_arrival_at.cmp(&st.next_any_idle_at) {
-        Less                                    => ,
-        _ if st.buf.iter().all(|x| x.is_none()) => (st.buf, st.buf_pointer),
-        _                                       => (st.buf.iter()
-                                                          .filter(|&x| x != st.buf.iter().max().unwrap())
-                                                          .cloned()
-                                                          .collect(),
-                                                          st.buf_pointer),
+        Less if st.buf.iter().all(|x| x.is_some()) => (),
+        Less                                       => (),
+        _ if st.buf.iter().all(|x| x.is_none())    => (st.buf, st.buf_pointer),
+        _                                          => (st.buf.iter()
+                                                             .filter(|&x| x != st.buf.iter().max().unwrap())
+                                                             .cloned()
+                                                             .collect(),
+                                                       st.buf_pointer),
     } 
+}
+
+fn add_to_buffer(st: State) -> Vec<Option<u64>> {
+    Vec::new()
 }
 
 fn get_next_event_and_time(st: State) -> (SimulationEvent, u64) {
