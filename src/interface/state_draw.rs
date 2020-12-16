fb50c4342388f572c30b7f8e4ed11ffca6628094
vegtamr_ <49475431+vegtamr-ab@users.noreply.github.com>
2020-12-16 04:06:03 +0300

    Tabs improved in state_draw

--- src/interface/state_draw.rs
+++ src/interface/state_draw.rs
@@ -8,8 +8,8 @@ const BOX_HEIGHT: i32 = 30;
 
 pub fn draw_sources(s: &types::Simulation) -> Result<(), Box<dyn std::error::Error>> {
     let root = BitMapBackend::new("target/stepmodeui/states.png",
-                                                            ((s.max_sources as u32 * BOX_WIDTH as u32), BOX_HEIGHT as u32))
-                                                            .into_drawing_area();
+                                  ((s.max_sources as u32 * BOX_WIDTH as u32), BOX_HEIGHT as u32))
+                                  .into_drawing_area();
     root.fill(&WHITE)?;
 
     for i in 0..s.max_sources {
@@ -17,7 +17,7 @@ pub fn draw_sources(s: &types::Simulation) -> Result<(), Box<dyn std::error::Err
         let style = TextStyle::from(("sans-serif", 20).into_font()).pos(pos).color(&BLACK);
 
         root.draw(&Rectangle::new([(i as i32 * BOX_WIDTH, 0), ((i + 1) as i32 * BOX_WIDTH - 1, BOX_HEIGHT - 1)], 
-                                    &BLACK))?;
+                                  &BLACK))?;
 
         root.draw(&Text::new(format!("{}", s.state.sources[i]), (i as i32 * BOX_WIDTH + BOX_WIDTH / 2, BOX_HEIGHT / 2), style))?;
     }
@@ -27,25 +27,25 @@ pub fn draw_sources(s: &types::Simulation) -> Result<(), Box<dyn std::error::Err
 
 pub fn draw_devices(s: &types::Simulation) -> Result<(), Box<dyn std::error::Error>> {
     let root = BitMapBackend::new("target/stepmodeui/devices.png",
-                                                            ((s.max_devices as u32 * BOX_WIDTH as u32), BOX_HEIGHT as u32))
-                                                            .into_drawing_area();
+                                  ((s.max_devices as u32 * BOX_WIDTH as u32), BOX_HEIGHT as u32))
+                                  .into_drawing_area();
     root.fill(&WHITE)?;
 
     for i in 0..s.max_devices {
         let mut border = Rectangle::new([(i as i32 * BOX_WIDTH, 0), 
-                                                                   ((i + 1) as i32 * BOX_WIDTH - 1, BOX_HEIGHT - 1)], 
-        &BLACK);
+                                        ((i + 1) as i32 * BOX_WIDTH - 1, BOX_HEIGHT - 1)], 
+                                        &BLACK);
         
         if s.state.device_pointer == i {
             border = Rectangle::new([(i as i32 * BOX_WIDTH, 0), ((i + 1) as i32 * BOX_WIDTH - 1, BOX_HEIGHT - 1)], 
-                                        BLUE.stroke_width(3));
+                                    BLUE.stroke_width(3));
         };
 
         if s.current_time >= s.state.devices[i] {
             root.draw(&border)?;
         } else {
             root.draw(&Rectangle::new([(i as i32 * BOX_WIDTH, 0), ((i + 1) as i32 * BOX_WIDTH - 1, BOX_HEIGHT - 1)],
-                                        RED.filled()))?;
+                                      RED.filled()))?;
             root.draw(&border)?;
         }
 
@@ -59,28 +59,29 @@ pub fn draw_devices(s: &types::Simulation) -> Result<(), Box<dyn std::error::Err
 
 pub fn draw_buffer(s: &types::Simulation) -> Result<(), Box<dyn std::error::Error>> {
     let root = BitMapBackend::new("target/stepmodeui/buffer.png",
-                                                            ((s.max_buf_length as u32 * BOX_WIDTH as u32), BOX_HEIGHT as u32))
-                                                            .into_drawing_area();
+                                  ((s.max_buf_length as u32 * BOX_WIDTH as u32), BOX_HEIGHT as u32))
+                                  .into_drawing_area();
     root.fill(&WHITE)?;
 
     for i in 0..s.max_buf_length {
         let mut border = Rectangle::new([(i as i32 * BOX_WIDTH, 0), 
-                                                                   ((i + 1) as i32 * BOX_WIDTH - 1, BOX_HEIGHT - 1)], 
-        &BLACK);
+                                        ((i + 1) as i32 * BOX_WIDTH - 1, BOX_HEIGHT - 1)], 
+                                        &BLACK);
         
         if s.state.buf_pointer == i {
             border = Rectangle::new([(i as i32 * BOX_WIDTH, 0), ((i + 1) as i32 * BOX_WIDTH - 1, BOX_HEIGHT - 1)], 
-                                        BLUE.stroke_width(3));
+                                    BLUE.stroke_width(3));
         };
 
         if s.state.buf[i].is_some() {
             let pos = Pos::new(HPos::Center, VPos::Center);
             let style = TextStyle::from(("sans-serif", 20).into_font()).pos(pos).color(&BLACK);
             root.draw(&Rectangle::new([(i as i32 * BOX_WIDTH, 0), ((i + 1) as i32 * BOX_WIDTH - 1, BOX_HEIGHT - 1)], 
-                                        RED.filled()))?;
+                                      RED.filled()))?;
             root.draw(&border)?;
 
-            root.draw(&Text::new(format!("{}", s.state.buf[i].unwrap().time_arrived), (i as i32 * BOX_WIDTH + BOX_WIDTH / 2, BOX_HEIGHT / 2), style))?;
+            root.draw(&Text::new(format!("{}", s.state.buf[i].unwrap().time_arrived),
+                                 (i as i32 * BOX_WIDTH + BOX_WIDTH / 2, BOX_HEIGHT / 2), style))?;
         } else {
             root.draw(&border)?;
         }
